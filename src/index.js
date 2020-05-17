e706d0debddf78c891760602a7bd5f453b7bbd2c
Tushaaharg1999 <tushargoyal414@gmail.com>
2020-05-17 17:00:55 +0530

    Initialize project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './service/serviceWorker/serviceWorker';
+import Home from './modules/home/home';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
